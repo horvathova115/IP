af2231bb5b90ba73e47bef0f4d79209cd0cfbdba
Luís Ornelas <34454359+lunaticluis@users.noreply.github.com>
2017-12-12 18:40:56 +0000

    Update Kingdom.java

--- src/Kingdom.java
+++ src/Kingdom.java
@@ -1,12 +1,23 @@
 public class Kingdom {
 
 	String name;
+	boolean active;
+	
 	
 	public Kingdom(String name) {
 		this.name = name;
+		active = true;
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void fallenKingdom() {
+		active = false;
 	}
 }
