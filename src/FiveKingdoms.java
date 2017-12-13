0b13e6a9d121d88b1bf9540123fe53a67aaaee17
Luís Ornelas <34454359+lunaticluis@users.noreply.github.com>
2017-12-13 15:58:40 +0000

    Update FiveKingdoms.java

--- src/FiveKingdoms.java
+++ src/FiveKingdoms.java
@@ -4,23 +4,34 @@ public class FiveKingdoms {
 	public static final int PRICE_CAV = 4;
 	public static final int PRICE_LAN_ESP = 2;
 	
+	private int maplenghtx;
+	private int maglenghty;
 	private Fortress[] fortresses;
 	private Kingdom[] kingdoms;
+	private Army[] armies;
 	public boolean gameOn;
 	private int counter;
-	public String teamPlaying; //still don't know if we need this
+	public String teamPlaying; //still don't know if we need this or not
+	private int numOfKingdoms;
+	private int numOfCastles;
 	private int currentKingdom;
-	private int nKingdoms;
 	private int currentCastle;
+	private int currentArmy;
 
-	public FiveKingdoms(int nKingdoms) {
-		kingdoms = new Kingdom[MAX_KINGDOMS];
-		fortresses = new Fortress[50];
-		gameOn = false;
+	public FiveKingdoms(int mapx, int mapy, int numOfKingdoms, int numOfCastles) {
+		this.maplenghtx = mapx;
+		this.maplenghty = mapy;
+		kingdoms = new Kingdom[numOfKingdoms];
+		fortresses = new Fortress[numOfCastles];
+		armies = new Army[numOfCastles];
+		gameOn = true;
 		counter = 0;
-		teamPlaying = ""; //still don't know if we need this
+		teamPlaying = ""; //still don't know if we need this or not
 		currentKingdom = -1;
-		this.nKingdoms = nKingdoms;
+		currentCastle = -1;
+		currentArmy = -1;
+		this.nKingdoms = numOfKingdoms;
+		this.nCastles = numOfCastles;
 	}
 	
 	public void initializeIteratorKingdoms() {
@@ -41,7 +52,7 @@ public class FiveKingdoms {
 	
 	public int activeKingdoms() {
 		int k = 0;
-		for (int i = 0; i == nKingdoms; i++) {
+		for (int i = 0; i == numOfKingdoms; i++) {
 			if (kingdoms[i].active == true) {
 				k++;
 			}
