5fd582c5d224a22952287b867b36393defcd90e6
horvathova115 <claire.horvathova@gmail.com>
2017-12-14 21:51:25 +0100

    Update Kingdom.java
    add new methods and variables

--- src/Kingdom.java
+++ src/Kingdom.java
@@ -2,11 +2,20 @@ public class Kingdom {
 
 	String name;
 	boolean active;
+	private Castles[] castles;
+	private int counter;
+	
+	private final int MAX=50;
+	private final int GROWTH=2;
+	private int current;
 	
 	
 	public Kingdom(String name) {
 		this.name = name;
 		active = true;
+		castles=new Castles[MAX];
+		counter=0;
+		current=-1;
 	}
 	
 	public String getName() {
@@ -20,4 +29,21 @@ public class Kingdom {
 	public void fallenKingdom() {
 		active = false;
 	}
-}
+	
+	public void addCastle(Castles castle){
+		this.castles[counter]=castle;
+	}
+
+	public void initializeIterator() {
+		current = 0;
+	}
+		
+	public boolean hasNext(){
+		return ((current >= 0 ) && (current < counter));
+	}
+	
+	public Castles next(){
+		return castles[current++];
+	}
+	
+ }
