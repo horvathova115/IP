ffb78d06b57fcfd482fd8ff89f5abaa1dcae26a7
horvathova115 <claire.horvathova@gmail.com>
2017-12-14 16:25:55 +0100

    Update FiveKingdoms.java
    map dimensions changed to Position type
    add 10000 new methods for work with new game

--- src/FiveKingdoms.java
+++ src/FiveKingdoms.java
@@ -4,9 +4,10 @@ public class FiveKingdoms {
 	public static final int PRICE_CAV = 4;
 	public static final int PRICE_LAN_ESP = 2;
 	
-	private int maplenghtx;
-	private int maglenghty;
-	private Fortress[] fortresses;
+	//private int maplenghtx;
+	//private int maglenghty;
+	private Position map;
+	private Castles[] castles;
 	private Kingdom[] kingdoms;
 	private Army[] armies;
 	public boolean gameOn;
@@ -17,21 +18,27 @@ public class FiveKingdoms {
 	private int currentKingdom;
 	private int currentCastle;
 	private int currentArmy;
+	private int counterKingdoms;
+	private int counterCastles;
+	
 
 	public FiveKingdoms(int mapx, int mapy, int numOfKingdoms, int numOfCastles) {
-		this.maplenghtx = mapx;
-		this.maplenghty = mapy;
+		//this.maplenghtx = mapx;
+		//this.maglenghty = mapy;
+		map = new Position(mapx,mapy);
 		kingdoms = new Kingdom[numOfKingdoms];
-		fortresses = new Fortress[numOfCastles];
+		castles = new Castles[numOfCastles];
 		armies = new Army[numOfCastles];
 		gameOn = true;
 		counter = 0;
+		counterCastles=0;
+		counterKingdoms=0;
 		teamPlaying = ""; //still don't know if we need this or not
 		currentKingdom = -1;
 		currentCastle = -1;
 		currentArmy = -1;
-		this.nKingdoms = numOfKingdoms;
-		this.nCastles = numOfCastles;
+		this.numOfKingdoms = numOfKingdoms;
+		this.numOfCastles = numOfCastles;
 	}
 	
 	public void initializeIteratorKingdoms() {
@@ -59,6 +66,100 @@ public class FiveKingdoms {
 		}
 		return k;
 	}
+
+	//what with theese methods in Five Kingdoms, which I cant reach while not initializing FiveKigdoms constructor...
+	public void addCastle(Castles c) {
+		// TODO Auto-generated method stub
+		castles[counter]=c;
+		counter++;
+	}
+
+	public void removeAllCastles() {
+		// TODO Auto-generated method stub
+		castles= new Castles[numOfCastles];
+	}
+
+	public boolean existCastlePos(int x, int y) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<counterCastles;i++){
+			if(castles[i].pos().getX()==x && castles[i].pos().getY()==y){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+	public boolean existCastleName(String name) {
+		// TODO Auto-generated method stub
+		boolean exist = false;
+		for(int i=0;i<counterCastles;i++){
+			if(castles[i].getName().equals(name)){
+				exist= true;
+			}
+		}
+		return exist;
+	}
+
+	public Position getMapPosition() {
+		// TODO Auto-generated method stub
+		return map;
+	}
+
+	public boolean hasNextCastle() {
+		// TODO Auto-generated method stub
+		return ((currentCastle<counterCastles) && (currentCastle>=0));
+	}
+
+	public Kingdom nextKingdom() {
+		// TODO Auto-generated method stub
+		return kingdoms[currentKingdom++];
+	}
+
+	public Castles nextCastle() {
+		// TODO Auto-generated method stub
+		return castles[currentCastle++];
+	}
+
+	public void addKingdom(Kingdom k) {
+		// TODO Auto-generated method stub
+		kingdoms[counterKingdoms]=k;
+	}
+
+	public void removeAllKingdoms() {
+		// TODO Auto-generated method stub
+		kingdoms = new Kingdom[numOfKingdoms];
+	}
+
+	public boolean existKingdom(String kingdom) {
+		// TODO Auto-generated method stub
+		boolean exist=false;
+		for(int i=0;i<numOfKingdoms;i++){
+			if(kingdoms[i].getName().equals(kingdom)){
+				exist=true;
+			}
+		}
+		return exist;
+	}
+
+	public boolean castleFree(String castle) {
+		// TODO Auto-generated method stub
+		boolean occupied=false;
+		for(int i=0;i<numOfCastles;i++){
+			Castles c= castles[i];
+			if(c.getName().equals(castle)){
+				if(c.isOccupied()){
+					occupied=true;
+				}
+			}
+		}
+		return occupied;
+	}
 	
-	
+	public int getNumberofCastles(){
+		return numOfCastles;
+	}
+	public int getNumberOfKingdoms(){
+		return numOfKingdoms;
+	}
 }
