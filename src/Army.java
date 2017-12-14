62c96970582be4b00d7a723d49c452c7e41331c5
Luís Ornelas <34454359+lunaticluis@users.noreply.github.com>
2017-12-14 19:47:20 +0000

    Update Army.java

--- src/Army.java
+++ src/Army.java
@@ -1,17 +1,21 @@
 public class Army {
 
 	String owner;
+	String castle;
 	String type;
-	int x;
-	int y;
 	
-	public Army(String owner, String type) {
+	public Army(String owner, String castle, String type) {
 		this.owner = owner;
+		this.castle = castle;
 		this.type = type;
 	}
 	
 	public String getOwner() {
 		return owner;
+	}
+	
+	public String getCastle() {
+		return castle;
 	}
 	
 	public String getType() {
