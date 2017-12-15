5709fbbe165a084c6ecb7377fd73d9eacc59e22a
Luís Ornelas <34454359+lunaticluis@users.noreply.github.com>
2017-12-15 07:39:47 +0000

    Update Army.java

--- src/Army.java
+++ src/Army.java
@@ -3,11 +3,13 @@ public class Army {
 	String owner;
 	String castle;
 	String type;
+	private Position z;
 	
-	public Army(String owner, String castle, String type) {
+	public Army(String owner, String castle, String type, int x, int y) {
 		this.owner = owner;
 		this.castle = castle;
 		this.type = type;
+		z=new Position(x,y);
 	}
 	
 	public String getOwner() {
@@ -20,5 +22,13 @@ public class Army {
 	
 	public String getType() {
 		return type;
+	}
+	
+	public Position getPos() {
+		return z;
+	}
+	
+	public void changePos(Position newest) {
+		z = newest;
 	}
 }
